607ab6f39cf05bc616a425c928d6362df261f29a
athakre311 <athakre311@gmail.com>
2024-11-24 22:36:50 +0530

    commit changes in loginTest and create new LoginPage and HomePage classes

--- src/test/java/com/abby/qa/testcases/LoginTest.java
+++ src/test/java/com/abby/qa/testcases/LoginTest.java
@@ -1,6 +1,8 @@
 package com.abby.qa.testcases;
 
+import com.abby.qa.pages.AccountPage;
 import com.abby.qa.pages.HomePage;
+import com.abby.qa.pages.LoginPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -19,14 +21,17 @@ public class LoginTest extends Base{
 	}
 	
 	WebDriver driver;
-	HomePage homePage;
+//	HomePage homePage;
+	LoginPage loginPage;
+	AccountPage accountPage;
 
 	@BeforeMethod
 	public void setup() {
 		driver = inisalizeBrowserOpenUrl(prop.getProperty("browser"));
-		homePage = new HomePage(driver);
+		HomePage homePage = new HomePage(driver);
 		homePage.clickOnMyAccount();
 		homePage.selectLoginOption();
+		loginPage = new LoginPage(driver);
 	}
 	
 	@AfterMethod
@@ -36,35 +41,35 @@ public class LoginTest extends Base{
 	
 	@Test (priority=1, dataProvider="supplyTestData")
 	public void TC_LF_001_loginWithValidCredentials(String email, String password) {
-		driver.findElement(By.id("input-email")).sendKeys(email);
-		driver.findElement(By.id("input-password")).sendKeys(password);
-		driver.findElement(By.xpath("//input[@value='Login']")).click();
-		Assert.assertTrue(driver.findElement(By.xpath("//li//a[contains(@href,'edit')]")).isDisplayed());
+		loginPage.enterEmail(email);
+		loginPage.enterPassword(password);
+		loginPage.clickonLoginButton();
+		accountPage = new AccountPage(driver);
+		Assert.assertTrue(accountPage.getExpectedMassage(),"Edit your account information option is not displayed");
 	}
 
 	@DataProvider
 	public Object[][] supplyTestData(){
-		Object[][] data = {{"amotooricap9@gmail.com","123445"},{"amotooricap41@gmail.com","123345"},{"amotooricap4@gmail.com","12345"}};
+		Object[][] data = {{"amotooricap4@gmail.com","12345"},{"amotooricap41@gmail.com","123345"},{"amotooricap4@gmail.com","12345"}};
 //		Object[][] data = Utilities.getTestDataFromExcel("Sheet1");
 		return data;
 	}
-	
+
 	@Test (priority=2)
 	public void TC_LF_002_loginWithInvalidCredentials() {
-		driver.findElement(By.id("input-email")).sendKeys(Utilities.getTimeStamp());
-		System.out.println(driver.findElement(By.id("input-email")).getAttribute("value"));
-		driver.findElement(By.id("input-password")).sendKeys(prop.getProperty("validPassword"));
-		driver.findElement(By.xpath("//input[@value='Login']")).click();
-		String actualErrorMassage = driver.findElement(By.xpath("//div[contains(@class,'alert')]")).getText();
+		loginPage.enterEmail(Utilities.getTimeStamp());
+		loginPage.enterPassword(prop.getProperty("validPassword"));
+		loginPage.clickonLoginButton();
+		String actualErrorMassage = loginPage.getwarningMassage();
 		Assert.assertTrue(actualErrorMassage.contains(dataprop.getProperty("expectedErrorMassage")), "Invalid error massage");
 	}
 
 	@Test (priority=2)
 	public void TC_LF_003_loginWithInvalidEmailValidPass() {
-		driver.findElement(By.id("input-email")).sendKeys(Utilities.getTimeStamp());
-		driver.findElement(By.id("input-password")).sendKeys(dataprop.getProperty("invalidPassword"));
-		driver.findElement(By.xpath("//input[@value='Login']")).click();
-		String actualErrorMassage = driver.findElement(By.xpath("//div[contains(@class,'alert')]")).getText();
+		loginPage.enterEmail(Utilities.getTimeStamp());
+		loginPage.enterPassword(dataprop.getProperty("invalidPassword"));
+		loginPage.clickonLoginButton();
+		String actualErrorMassage = loginPage.getwarningMassage();
 		Assert.assertTrue(actualErrorMassage.contains(dataprop.getProperty("expectedErrorMassage")), "Invalid error massage");
 	}
 
